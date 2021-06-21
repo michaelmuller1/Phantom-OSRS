6f97b9ef5a78b76f8f76e76deca252c75d73816d
mullermi <gmuller4028@gmail.com>
2021-06-21 16:23:30 -0400

    test test test

--- src/test/java/com/helmetcheck/helmetcheckPluginTest.java
+++ src/test/java/com/helmetcheck/helmetcheckPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.helmetcheck;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class helmetcheckPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(helmetcheckPlugin.class);
 		RuneLite.main(args);
 	}
 }
